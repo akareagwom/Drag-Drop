74d477a5902bb4f77d68f1342f1636585bb149fb
akareagwom <akareazikang20@gmail.com>
2024-08-15 15:40:26 +0100

    modalText

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -4,17 +4,18 @@ import React, { useState } from 'react';
 
 const Cards = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const [modaLText,ModalText]= useState('')
+    const [modaLText,setModalText]= useState('')
 
     const openModal =()=>{
         setIsOpen(true);
+        setModalText('This is a modal');
     }
   return (
     <div>
         <button onClick={openModal}>add new card</button>
         {isOpen && 
         <div className=' w-[400px] rounded h-[30vh] text-white bg-red-400'>
-          hola senior
+          {modaLText}
         </div>
         }
     </div>
