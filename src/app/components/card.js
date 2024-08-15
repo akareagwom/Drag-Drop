bc109358ff45ad60f019a52e92131fa9cd48f6cd
akareagwom <akareazikang20@gmail.com>
2024-08-15 16:55:05 +0100

    text modal

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -5,20 +5,21 @@ import Draggable from 'react-draggable';
 
 const Cards = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [openText, setOpenText] = useState(false);
     const [modaLText,setModalText]= useState('');
     const [cards, setCards] = useState([]);
 
     const openModal =()=>{
         setIsOpen(true);
-        setModalText('This is a modal');
+        // setModalText('');
     }
     const closeModal =()=>{
         setIsOpen(false);
     }
 
     const handleAddText = () => {
-        if (text.trim()) {
-          setCards([...cards, text]);
+        if (modaLText.trim()) {
+          setCards([...cards, modaLText]);
           setModalText('');
           closeModal();
         }
@@ -29,7 +30,10 @@ const Cards = () => {
         {isOpen && 
         <div className=' flex p-2 flex-col w-[400px] rounded h-[30vh] text-black bg-red-400'>
           
-            <textarea/>
+            <textarea 
+            value={modaLText}
+            onChange={(e) => setModalText(e.target.value)}
+            />
           <button className='bg-white mt-12 w-[100px] ml-[70%] text-black rounded p-2' onClick={closeModal}>close</button>
             <button onClick={handleAddText} style={{ marginRight: '10px' }}>
               Add
@@ -39,7 +43,13 @@ const Cards = () => {
         <div>
             <Draggable>
                 <div>
-                     you can drag me
+                {cards.map((card, index) => (
+                <div key={index}>
+                    {card.slice(0, 10)} more...
+                    <button onClick={() => openModal(modaLText)}>Show More</button>
+                    
+                </div>
+                ))}
                 </div>
             </Draggable>
         </div>
