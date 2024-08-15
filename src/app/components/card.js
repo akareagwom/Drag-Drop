a65b09c8fc5850f6544260137d64034c617a0dde
akareagwom <akareazikang20@gmail.com>
2024-08-15 16:02:31 +0100

    setting cards

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -1,10 +1,12 @@
 "use client"
 import React, { useState } from 'react';
+import Draggable from 'react-draggable';
 
 
 const Cards = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const [modaLText,setModalText]= useState('')
+    const [modaLText,setModalText]= useState('');
+    const [cards, setCards] = useState([]);
 
     const openModal =()=>{
         setIsOpen(true);
@@ -17,11 +19,18 @@ const Cards = () => {
     <div>
         <button onClick={openModal}>add new card</button>
         {isOpen && 
-        <div className=' w-[400px] rounded h-[30vh] text-white bg-red-400'>
+        <div className=' flex p-2 flex-col w-[400px] rounded h-[30vh] text-white bg-red-400'>
           {modaLText}
-          <button onClick={closeModal}>close</button>
+          <button className='bg-white mt-12 w-[100px] ml-[70%] text-black rounded p-2' onClick={closeModal}>close</button>
         </div>
         }
+        <div>
+            <Draggable>
+                <div>
+                     you can drag me
+                </div>
+            </Draggable>
+        </div>
     </div>
       );
 };
