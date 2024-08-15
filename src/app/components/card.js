d005b8b7147edbe6a11da81f793b7e14e73cfe2c
akareagwom <akareazikang20@gmail.com>
2024-08-15 15:04:20 +0100

    addcard component

---
+++ src/app/components/card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Cards = () => {
+  return (
+    <div className=' w-[400px] rounded h-[30vh] text-white bg-red-400'>
+      hola senior
+    </div>
+  );
+};
+
+export default Cards;
